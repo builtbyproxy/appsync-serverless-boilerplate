b0995a5337bbae8fc1f0fee8dff057bc30b0c4be
Lachlan Young <lachlan_young@hotmail.com>
2019-06-19 16:00:04 +0200

    cleanup and switchCase

--- serverless/handler.js
+++ serverless/handler.js
@@ -1,14 +1,41 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+exports.graphqlHandler = async (event, context, callback) => {
+    console.log('hit event: ', event);
+    switch (event.field) {
+        case "createUser": {
+            try{
+                console.log('creating a user: ', body);
+                return callback(null, JSON.stringify({success: true}));
+            } catch(err){
+                console.log('error creatingUser: ', err);
+                callback(err, null);
+            }
+        }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+        case "updateUser": {
+            try{
+                console.log('updating a user: ', body);
+                return callback(null, [JSON.stringify({success: true})]);
+            } catch(err){
+                console.log('error updating user: ', err);
+                callback(err, null);
+            }
+        }
+
+        case "getUser": {
+            try {
+                console.log('getting a user: ', body);
+                return callback(null, JSON.stringify({success: true}));
+            } catch(err){
+                console.log('error getting user: ', err);
+                callback(err, null);
+            }
+        }
+        
+        default: {
+            callback(`Unknown field, unable to resolve ${event.field}`, null);
+            break;
+        }
+    }
 };
